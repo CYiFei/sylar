d74f1cd083421a63f7362e7b21f64fa064f4e6ee
chengyifei <3521315108@qq.com>
2022-08-18 16:29:11 +0800

    日志模块整合 	modified:   sylar/log.cc 	modified:   sylar/log.h 	modified:   sylar/thread.h 	modified:   tests/test_thread.cc

--- sylar/thread.h
+++ sylar/thread.h
@@ -8,6 +8,7 @@
 #include <semaphore.h>
 #include <stdint.h>
 #include <string>
+#include <atomic>
 
 //pthread_xxx
 //std::thread, pthread
@@ -121,6 +122,36 @@ private:
     bool m_locked;
 };
 
+class Mutex {
+public:
+    typedef ScopedLockImpl<Mutex> Lock;
+    Mutex() {
+        pthread_mutex_init(&m_mutex, nullptr);
+    }
+
+    ~Mutex() {
+        pthread_mutex_destroy(&m_mutex);
+    }
+
+    void lock() {
+        pthread_mutex_lock(&m_mutex);
+    }
+
+    void unlock() {
+        pthread_mutex_unlock(&m_mutex);
+    }
+private:
+    pthread_mutex_t m_mutex;
+};
+
+class NullMutex {
+public:
+    typedef ScopedLockImpl<NullMutex> Lock;
+    NullMutex() {}
+    ~NullMutex() {}
+    void lock() {}
+    void unlock() {}
+};
 
 class RWMutex {
 public:
@@ -148,6 +179,61 @@ public:
     }
 private:
     pthread_rwlock_t m_lock;
+};
+
+class NullRWMutex {
+public:
+    typedef ReadScopedLockImpl<NullMutex> ReadLock;
+    typedef WriteScopedLockImpl<NullMutex> WriteLock;
+
+    NullRWMutex() {}
+    ~NullRWMutex() {}
+
+    void rdlock() {}
+    void wrlock() {}
+    void unlock() {}
+};
+
+class Spinlock {
+public:
+    typedef ScopedLockImpl<Spinlock> Lock;
+    Spinlock() {
+        pthread_spin_init(&m_mutex, 0);
+    }
+
+    ~Spinlock() {
+        pthread_spin_destroy(&m_mutex);
+    }
+
+    void lock() {
+        pthread_spin_lock(&m_mutex);
+    }
+
+    void unlock() {
+        pthread_spin_unlock(&m_mutex);
+    }
+private:
+    pthread_spinlock_t m_mutex;
+};
+
+class CASLock {
+public:
+    typedef ScopedLockImpl<CASLock> Lock;
+    CASLock() {
+        m_mutex.clear();
+    }
+    ~CASLock() {
+    }
+
+    void lock() {
+        while(std::atomic_flag_test_and_set_explicit(&m_mutex, std::memory_order_acquire));
+    }
+
+    void unlock() {
+        std::atomic_flag_clear_explicit(&m_mutex, std::memory_order_release);
+    }
+private:
+    volatile std::atomic_flag m_mutex;
 };
 
 class Thread {
